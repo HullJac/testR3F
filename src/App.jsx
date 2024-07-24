37539ac15a2ae6b61e1c58bf2e1248bd2b6e8fa1
HullJac <jacobhull55@gmail.com>
2024-07-24 10:01:00 -0500

    Added torus shapes and changed background color.

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,39 @@ const Cube = ({position, size, color}) => {
 }
 
 const Sphere = ({position, size, color}) => {
+    const ref = useRef();
+    useFrame((state,delta) =>{
+        ref.current.rotation.x += delta;
+        ref.current.rotation.y += delta*2;
+        ref.current.position.z = Math.sin(state.clock.elapsedTime) * 2;
+    });
+    return (
+        <mesh position={position} ref={ref}>
+            <sphereGeometry args={size}/>
+            <meshStandardMaterial color={color} wireframe/>
+        </mesh>
+    );
+}
+
+const Torus = ({position, size, color}) => {
     return (
         <mesh position={position}>
-            <sphereGeometry args={size}/>
+            <torusGeometry args={size}/>
+            <meshStandardMaterial color={color}/>
+        </mesh>
+    );
+}
+
+const TorusKnot = ({position, size, color}) => {
+    const ref = useRef();
+    useFrame((state,delta) =>{
+        ref.current.rotation.x += delta;
+        ref.current.rotation.y += delta*2;
+        ref.current.position.z = Math.sin(state.clock.elapsedTime) * 2;
+    });
+    return (
+        <mesh position={position} ref={ref}>
+            <torusKnotGeometry args={size}/>
             <meshStandardMaterial color={color}/>
         </mesh>
     );
@@ -34,7 +64,9 @@ const App = () => {
         <directionalLight position={[0 , 0, 2]} intensity={0.7}/>
         <ambientLight intensity={0.1}/>
         <Cube position={[2,1,0]} size={[1,1,1]} color={"blue"} />
-        <Sphere position={[0,0,0]} args={[1,30,30]} color={"pink"}/>
+        <Sphere position={[0,0,0]} size={[1,30,30]} color={"pink"}/>
+        <Torus position={[2,0,0]} size={[0.5,0.1,30,30]} color={"red"}/>
+        <TorusKnot position={[-2,0,0]} size={[0.5,0.1,1000,50]} color={"orange"}/>
     </Canvas>
   );
 }

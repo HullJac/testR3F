b0bbff1fc434ba1dcf11d10a6088a0634872c060
HullJac <jacobhull55@gmail.com>
2024-08-26 21:41:44 -0500

    Added Drei to the the project.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import { MeshStandardMaterial } from "three"
 import { useRef, useState } from "react"
 import { color, sin } from "three/examples/jsm/nodes/Nodes.js"
+import { MeshWobbleMaterial, OrbitControls } from "@react-three/drei"
+
 
 const Cube = ({position, size, color}) => {
     const ref = useRef();
@@ -57,15 +59,16 @@ const Torus = ({position, size, color}) => {
 
 const TorusKnot = ({position, size, color}) => {
     const ref = useRef();
-    useFrame((state,delta) =>{
-        ref.current.rotation.x += delta;
-        ref.current.rotation.y += delta*2;
-        ref.current.position.z = Math.sin(state.clock.elapsedTime) * 2;
-    });
+    // useFrame((state,delta) =>{
+    //     ref.current.rotation.x += delta;
+    //     ref.current.rotation.y += delta*2;
+    //     ref.current.position.z = Math.sin(state.clock.elapsedTime) * 2;
+    // });
     return (
         <mesh position={position} ref={ref}>
             <torusKnotGeometry args={size}/>
-            <meshStandardMaterial color={color}/>
+            {/* <meshStandardMaterial color={color}/> */}
+            <MeshWobbleMaterial factor={5} speed={.5} color={color}/>
         </mesh>
     );
 }
@@ -76,9 +79,10 @@ const App = () => {
         <directionalLight position={[0 , 0, 2]} intensity={0.7}/>
         <ambientLight intensity={0.1}/>
         {/* <Cube position={[2,1,0]} size={[1,1,1]} color={"blue"} /> */}
-        <Sphere position={[0,0,0]} size={[1,30,30]} color={"red"}/>
+        {/* <Sphere position={[0,0,0]} size={[1,30,30]} color={"red"}/> */}
         {/* <Torus position={[2,0,0]} size={[0.5,0.1,30,30]} color={"yellow"}/> */}
-        {/* <TorusKnot position={[-2,0,0]} size={[0.5,0.1,1000,50]} color={"orange"}/> */}
+        <TorusKnot position={[0,0,0]} size={[1,0.1,1000,50]} color={"white"}/>
+        <OrbitControls enableZoom={false}/>
     </Canvas>
   );
 }
